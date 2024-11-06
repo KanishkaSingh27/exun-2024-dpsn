d618a5889f2159e994b19464e965d0ecb1de7086
KanishkaSingh27 <69956807+KanishkaSingh27@users.noreply.github.com>
2024-11-06 20:39:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,17 +58,17 @@ loader.load('MAP.glb', (gltf) => {
     { 
       position: new THREE.Vector3(-0.45, 1.1, -1.09), 
       name: 'Ancient Ruins', 
-      url: 'dashboard.html' 
+      url: 'index.html' 
     },
     { 
       position: new THREE.Vector3(1.95, 1.1, 0.5), 
       name: 'Research Base Alpha', 
-      url: 'forum.html' 
+      url: 'index.html' 
     },
     { 
       position: new THREE.Vector3(3.95, 1.1, 0.5), 
       name: 'Research Base Alpha', 
-      url: 'sign in.html' 
+      url: 'index.html' 
     },
   ];
 
